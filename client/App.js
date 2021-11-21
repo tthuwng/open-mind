a09f50d5c024cf5a453ee392e4e60cace10ab2c5
Viet Bui <buiminhviet.2002@gmail.com>
2021-11-21 13:14:10 -0600

    Update App.js

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! TryOK</Text>
+      <Text>Open Mind</Text>
       <StatusBar style="auto" />
     </View>
   );
